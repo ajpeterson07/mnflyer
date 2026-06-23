02af0de9a8ae01faa8695aac74fd2913ca233c3e
Aaron Peterson <pet03315@umn.edu>
2026-06-23 10:19:47 -0500

    Cleanup: Remove core, modules, and site files from tracking

--- web/modules/contrib/advancedqueue/src/Exception/InvalidBackendException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\advancedqueue\Exception;
-
-/**
- * Exception thrown when a backend is not suitable for use with a job.
- */
-class InvalidBackendException extends \Exception {}
